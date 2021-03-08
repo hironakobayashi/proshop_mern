1f99cba61349586558635011d94ddee305c65012
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-03-09 08:37:26 +0900

    Product Details Reducer & Action

--- frontend/src/actions/productAction.ts
+++ frontend/src/actions/productAction.ts
@@ -4,23 +4,23 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstant'
 import { IProduct } from '../interfaces'
 
 interface ProductListRequestAction {
   type: typeof PRODUCT_LIST_REQUEST
 }
-
 interface ProductListSuccessAction {
   type: typeof PRODUCT_LIST_SUCCESS
   payload: IProduct[]
 }
-
 interface ProductListFailAction {
   type: typeof PRODUCT_LIST_FAIL
   payload: string
 }
-
 export type ProductListActionTypes =
   | ProductListRequestAction
   | ProductListSuccessAction
@@ -38,6 +38,40 @@ export const listProducts = () => async (dispatch: Dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
+}
+
+interface ProductDetailsRequestAction {
+  type: typeof PRODUCT_DETAILS_REQUEST
+}
+interface ProductDetailsSuccessAction {
+  type: typeof PRODUCT_DETAILS_SUCCESS
+  payload: IProduct
+}
+interface ProductDetailsFailAction {
+  type: typeof PRODUCT_DETAILS_FAIL
+  payload: string
+}
+export type ProductDetailsActionTypes =
+  | ProductDetailsRequestAction
+  | ProductDetailsSuccessAction
+  | ProductDetailsFailAction
+
+export const listProductDetails = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message ? error.response.data.message : error.message,
     })
--- frontend/src/reducers/productReducers.ts
+++ frontend/src/reducers/productReducers.ts
@@ -1,13 +1,16 @@
 import { Reducer } from 'redux'
-import { ProductListActionTypes } from '../actions/productAction'
+import { ProductDetailsActionTypes, ProductListActionTypes } from '../actions/productAction'
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from '../constants/productConstant'
 import { IProduct } from '../interfaces'
 
-const initialState = {
+const initialListState = {
   loading: false,
   error: '',
   products: [
@@ -32,7 +35,7 @@ export type ProductListState = {
 }
 
 export const productListReducer: Reducer<ProductListState, ProductListActionTypes> = (
-  state = initialState,
+  state = initialListState,
   action: ProductListActionTypes
 ) => {
   switch (action.type) {
@@ -41,6 +44,44 @@ export const productListReducer: Reducer<ProductListState, ProductListActionType
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+const initialDetailsState = {
+  loading: false,
+  error: '',
+  product: {
+    _id: '',
+    name: '',
+    image: '',
+    description: '',
+    brand: '',
+    category: '',
+    price: 0,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+}
+export type ProductDetailsState = {
+  loading: boolean
+  product?: IProduct
+  error?: string
+}
+
+export const productDetailsReducer: Reducer<ProductDetailsState, ProductDetailsActionTypes> = (
+  state = initialDetailsState,
+  action: ProductDetailsActionTypes
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
