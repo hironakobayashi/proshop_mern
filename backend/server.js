782203bc813c5f44a29e01a98618b3b28c940b09
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-03-11 08:43:53 +0900

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,20 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoute from './routes/productRoutes.js'
+import userRoute from './routes/userRoutes.js'
 
 config()
 connectDB()
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running...!!')
 })
 
 app.use('/api/products', productRoute)
+app.use('/api/users', userRoute)
 
 app.use(notFound)
 
