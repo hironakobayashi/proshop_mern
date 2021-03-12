beeb9eb63af15d6307178496c7573235e449fdb4
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-03-12 10:20:01 +0900

    User Register Reducer, Action & Screen

--- frontend/src/actions/userActions.ts
+++ frontend/src/actions/userActions.ts
@@ -5,6 +5,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants'
 import { IUserInfo } from '../interfaces'
 
@@ -59,4 +62,54 @@ export const login = (email: string, password: string) => async (dispatch: Dispa
 export const logout = () => (dispatch: Dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
+}
+
+interface UserRegisterRequestAction {
+  type: typeof USER_REGISTER_REQUEST
+}
+interface UserRegisterSuccessAction {
+  type: typeof USER_REGISTER_SUCCESS
+  payload: IUserInfo
+}
+interface UserRegisterFailAction {
+  type: typeof USER_REGISTER_FAIL
+  payload: string
+}
+export type UserRegisterActionTypes =
+  | UserRegisterRequestAction
+  | UserRegisterSuccessAction
+  | UserRegisterFailAction
+
+export const register = (name: string, email: string, password: string) => async (
+  dispatch: Dispatch
+) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    const { data } = await axios.post('/api/users', { name, email, password }, config)
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
 }
--- frontend/src/reducers/userReducers.ts
+++ frontend/src/reducers/userReducers.ts
@@ -1,10 +1,13 @@
 import { Reducer } from 'redux'
-import { UserLoginActionTypes } from '../actions/userActions'
+import { UserLoginActionTypes, UserRegisterActionTypes } from '../actions/userActions'
 import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants'
 import { IUserInfo } from '../interfaces'
 
@@ -37,6 +40,38 @@ export const userLoginReducer: Reducer<UserLoginState, UserLoginActionTypes> = (
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
       return {}
+    default:
+      return state
+  }
+}
+
+const initialUserRegisterState = {
+  loading: false,
+  error: '',
+  userInfo: {
+    name: '',
+    email: '',
+    isAdmin: false,
+    token: '',
+  },
+}
+export type UserRegisterState = {
+  loading?: boolean
+  userInfo?: IUserInfo
+  error?: string
+}
+
+export const userRegisterReducer: Reducer<UserRegisterState, UserRegisterActionTypes> = (
+  state = initialUserRegisterState,
+  action: UserRegisterActionTypes
+) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
