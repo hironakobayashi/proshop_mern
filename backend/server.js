58875cc985ab3a49e326e0aec9f5480ff757e3c7
Hirona Kobayashi <kobayashihirona@team-lab.com>
2021-03-14 10:22:42 +0900

    Image Uplaod Config & Upload

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
+import path from 'path'
 import express from 'express'
 import { config } from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-import productRoute from './routes/productRoutes.js'
-import userRoute from './routes/userRoutes.js'
-import orderRoute from './routes/orderRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 config()
 connectDB()
@@ -18,14 +20,17 @@ app.get('/', (req, res) => {
   res.send('API is running...!!')
 })
 
-app.use('/api/products', productRoute)
-app.use('/api/users', userRoute)
-app.use('/api/orders', orderRoute)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-app.use(notFound)
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
